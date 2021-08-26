05f05071171db5d35e7bd95daa2be985054c64fd
Wen Xia <wen.xia@wework.com>
2021-08-26 18:21:06 +0800

    fix: cors

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,9 +1,13 @@
 import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
 import router from './router';
 import App from './app.vue';
 
+import 'element-plus/dist/index.css';
+
 const app = createApp(App);
 
+app.use(ElementPlus);
 app.use(router);
 
 app.mount('#app');
--- client/src/pages/Login/index.vue
+++ client/src/pages/Login/index.vue
@@ -1,9 +1,38 @@
-<template>login</template>
+<template>
+  <el-form ref="form" :model="form" label-width="80px">
+    <el-form-item label="账号">
+      <el-input v-model="username"></el-input>
+    </el-form-item>
+    <el-form-item label="密码">
+      <el-input v-model="password"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="handleLogin">登录</el-button>
+    </el-form-item>
+  </el-form>
+</template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import fetch from '@/fetch';
+
+console.log(fetch);
 
 export default defineComponent({
   name: 'Login',
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    handleLogin() {
+      fetch.post('/api/login', {
+        username: this.username,
+        password: this.password,
+      });
+    },
+  },
 });
 </script>
